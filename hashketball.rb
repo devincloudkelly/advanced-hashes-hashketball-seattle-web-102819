f17592912e5ecc150ada73648d814b62a5f4652a
Devin Kelly <devin.cloud.kelly@gmail.com>
2019-10-19 23:14:05 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -228,8 +228,8 @@ end
 
 def big_shoe_rebounds
   game_hash.each do |location, team|
-    team.each do |attribute, info|
-      info.each do |k, v|
+    team.each do |attribute, data|
+      data.each do |k, v|
        binding.pry
       end
     end

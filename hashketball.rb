a395528abf9bb23705d2c11c83213287c1812b3f
Devin Kelly <devin.cloud.kelly@gmail.com>
2019-10-19 23:46:43 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -246,11 +246,10 @@ end
 
 def rebounds_for_largest_shoe_size(game)
   max_player = nil
-  game.each do |team, team_hash|
+  game_hash.each do |team, team_hash|
     team_hash[:players].each do |player, player_hash|
       max_player ||= player_hash
       max_player = player_hash if player_hash[:shoe_size] > max_player[:shoe_size]
-    end
     end
   end
 

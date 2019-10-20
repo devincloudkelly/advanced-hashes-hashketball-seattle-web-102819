5a6e7a2062c2b74ea0060bbbd9536cda6f2e771f
Devin Kelly <devin.cloud.kelly@gmail.com>
2019-10-20 00:00:05 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -233,10 +233,8 @@ def big_shoe_rebounds
     team.each do |attribute, data|
       if attribute == :players
         data.each do |player|
-          binding.pry
-          player.each do |key, value|
-            if key == :shoe
-             biggest_shoe = value.max
+          if player[:shoe] > biggest_shoe
+           biggest_shoe = player[:shoe]
             end
           end
         end
